960db929e64f235cd5b6a57f30c659fff8a50f46
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-05-30 17:32:43 -0400

    Boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+    className?: string
 }
 
-export default App;
+const Styles = styled.div``
+
+export const App = ({ className }: Props) => {
+    return <Styles className={classNames(className)}>App</Styles>
+}
