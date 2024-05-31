7424dbd2f49e2152939aa3e876aef2cc4b9a6f6a
Eliyar Mamatjan <eliyar_m@hotmail.com>
2024-05-30 20:20:44 -0400

    Updated page title

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import styled from 'styled-components'
 
 import { AlphabetsList } from './components/AlphabetsList'
+import { UighurTextStyles } from './components/common/styles'
 
 interface Props {
     className?: string
@@ -12,7 +13,26 @@ const Styles = styled.div``
 export const App = ({ className }: Props) => {
     return (
         <Styles className={classNames(className)}>
+            <Header />
             <AlphabetsList />
         </Styles>
+    )
+}
+
+const HeaderStyles = styled.div`
+    margin: 24px 0 50px 0;
+    font-size: 60px;
+    font-weight: 600;
+    text-align: center;
+    color: #666;
+`
+
+const Header = () => {
+    return (
+        <HeaderStyles>
+            <span>Elipbe</span>
+            <span>&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;</span>
+            <UighurTextStyles>ئيلىپبە</UighurTextStyles>
+        </HeaderStyles>
     )
 }
--- src/components/common/styles.ts
+++ src/components/common/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const UighurTextStyles = styled.div`
+export const UighurTextStyles = styled.span`
     font-family: 'UighurFontBold', sans-serif;
     text-align: right;
 `
